e50bec6c9d07e7a2ef9730ff801205046cd68020
Bhargva <bhargavagonugunta123@gmail.com>
2024-03-16 01:59:14 +0530

    final chainges

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,23 @@ import Home from "./components/home/Home";
 import Specials from "./components/specials/Specials";
 import AboutUsPage from "./components/Aboutus/AboutUsPage";
 import ContactUsPage from "./components/contactus/ContactUsPage";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/home/header/Header.tsx";
 import Footer from "./components/home/footer/Footer.tsx";
+import GiftBoxPage from "./components/home/giftbox/giftbox-comp/GiftBoxPage.tsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Route path={"/"} element={<Home />} />
-      <Route path={"/shop"} element={<Specials />} />
-      <Route path={"/about"} element={<AboutUsPage />} />
-      <Route path={"/contactus"} element={<ContactUsPage />} />
-      <Route path={"/contactus"} element={<Specials />} />
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/shop"} element={<Specials />} />
+        <Route path={"/about"} element={<AboutUsPage />} />
+        <Route path={"/contactus"} element={<ContactUsPage />} />
+        <Route path={"/contactus"} element={<Specials />} />
+        <Route path={"/gifts"} element={<GiftBoxPage />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
   );
