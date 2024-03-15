ca76cf5176bb14e3df85b7e80329d1ccf99a5f92
Bhargva <bhargavagonugunta123@gmail.com>
2024-03-16 01:55:27 +0530

    final chainges

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import Home from "./components/home/Home";
 import Specials from "./components/specials/Specials";
 import AboutUsPage from "./components/Aboutus/AboutUsPage";
 import ContactUsPage from "./components/contactus/ContactUsPage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./components/home/header/Header.tsx";
 import Footer from "./components/home/footer/Footer.tsx";
-import GiftBoxPage from "./components/home/giftbox/giftbox-comp/GiftBoxPage.tsx";
 
 function App() {
   return (
