b79cdc6ce509d2875ce46b4b8833fd5e14d810e3
ashokvlack <rayalaashok75@gmail.com>
2024-04-08 14:12:42 +0530

    responsive added to cart,header,footerand links added wherever required

--- src/components/specials/square-card/SquareCard.tsx
+++ src/components/specials/square-card/SquareCard.tsx
@@ -1,7 +1,7 @@
 import SquareCardComp from "./square-card-comp/SquareCardComp";
 export default function SquareCard() {
   return (
-    <div className="px-10 py-24">
+    <div className=" py-24">
       <SquareCardComp />
     </div>
   );
