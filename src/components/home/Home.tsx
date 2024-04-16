30427a039d2b22cda67ee97eda723341c793cac2
Bhargva <bhargavagonugunta123@gmail.com>
2024-04-16 18:50:51 +0530

    img quality  chainges

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -5,11 +5,13 @@ import OrderItems from "./order-items/OrderItems.tsx";
 import GiftBox from "./giftbox/GiftBox.tsx";
 import ExperienceSairam from "./experience/experience-comp/ExperiencePage.tsx";
 import SquareCard from "./square-card/square-card-comp/SquareCardComp.tsx";
+import Crousal from "./carousel/Crousal.tsx";
+import { crouselcomponents } from "./carousel/CarouselData.tsx";
 
 export default function Home() {
   return (
     <div className="main_head">
-      {/*<Crousal />*/}
+      <Crousal components={crouselcomponents} />
       <ExperienceSairam />
       <div className={`p-5`}>
         <SquareCard />
